347a74087d59778fde09723c0262c8cce055f157
litlit2323 <60347171+litlit2323@users.noreply.github.com>
2020-01-27 17:25:05 +0500

    Hello добавил

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ export default class App extends Component {
         return (
             <div className="App">
                 <button onClick={()=>this.setState({num: this.state.num + 1})}>{this.state.num}</button>
+              <h1>Hello!</h1>
             </div>
         )
     }
