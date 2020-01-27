caf1a9ce11997a38d0f00ebe15bef75503e60e2e
100pecheneK <mistermihail23@gmail.com>
2020-01-27 18:26:04 +0500

    Создал кнопочка

--- src/App.js
+++ src/App.js
@@ -15,14 +15,10 @@ export default class App extends Component {
         const btn = !this.state.h1 ? "Hello" : "World";
         return (
             <div className="App">
-                <button onClick={()=>this.setState({num: this.state.num + 1})}>{this.state.num}</button>
-              <h1>Hello!</h1>
-            <div className="App">
                 <button onClick={() => this.setState({num: this.state.num + 1})}>{this.state.num}</button>
                 <h1>{h1}</h1>
                 <button onClick={() => this.setState({h1: !this.state.h1})}>{btn}</button>
-                <button onClick={()=>this.setState({run: this.state.run + "RUN "})}>{this.state.run}</button>
-
+                <button onClick={() => this.setState({run: this.state.run + "RUN "})}>{this.state.run}</button>
             </div>
         )
     }
