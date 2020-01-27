404ce194522aac82beb70bfd4ad8c7bfa54fab2c
litlit2323 <60347171+litlit2323@users.noreply.github.com>
2020-01-27 18:17:13 +0500

    добавил

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 
 export default class App extends Component {
     state = {
-        num: 0
+        num: 0,
+        run: ""
     };
 
     render() {
+
         return (
             <div className="App">
                 <button onClick={()=>this.setState({num: this.state.num + 1})}>{this.state.num}</button>
               <h1>Hello!</h1>
+                <button onClick={()=>this.setState({run: this.state.run + "RUN "})}>{this.state.run}</button>
             </div>
         )
     }
